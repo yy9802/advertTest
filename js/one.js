213d32e6978d264d7af1bee5a487584473d60862
Yingying Yang <yinyang3@wkmcn5080448.global.publicisgroupe.net>
2019-07-16 16:00:35 +0800

    第二次提交

--- js/one.js
+++ js/one.js
@@ -2,18 +2,12 @@ function isInSight(el) { //是否在可视区
     const bound = el.getBoundingClientRect();
     //只考虑向下滚动
     const clientHeight = window.innerHeight;
-    return bound.top <= clientHeight + 50; //提前加载
+    return bound.top <= clientHeight + 100; //提前加载
 }
 
 let index = 0; //已经加载图片的index
 function checkImgs() {
-    const imgs = document.querySelectorAll('.detailImg');
-    // Array.from(imgs).forEach(element => {
-    //     if (isInsight(element)) { //判断是否在可视区域
-    //         //加载图片
-    //         loadImg(element)
-    //     }
-    // });
+    const imgs = document.querySelectorAll('.lazyImg');
     for (let i = index; i < imgs.length; i++) {
         if (isInSight(imgs[i])) {
             loadImg(imgs[i]);
@@ -23,9 +17,11 @@ function checkImgs() {
 }
 
 function loadImg(el) {
-    if (!el.src) {
-        const source = el.dataset.src;
+    if (el.alt != '') {
+        const source = el.alt;
         el.src = source;
+        el.alt = '';
+        el.style.width = '100%'
     }
 }
 
